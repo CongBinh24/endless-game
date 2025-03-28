423652a634f267b04c392613ca62e88e4b17f31b
CongBinh24 <ncb240600@gmail.com>
2025-03-28 21:52:25 +0700

    Player's Animator states handle
    Player's Animator states handle

--- Assets/Endless Game/Scripts/Data/Player.cs
+++ Assets/Endless Game/Scripts/Data/Player.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static DataStruct;
 
 public class Player : MonoBehaviour, ICompChk
 {
@@ -14,6 +15,7 @@ public class Player : MonoBehaviour, ICompChk
     private Animator m_anim;
     private int m_blockId;
     private bool m_isDead;
+    private bool m_isOnBlock;
     private Vector2 m_centerPos;
 
 
@@ -26,22 +28,61 @@ public class Player : MonoBehaviour, ICompChk
     // Update is called once per frame
     void Update()
     {
-        
+        if (m_isDead || IsComponentsNull()) return;
+
+        Jump();
+
+        if (m_rb.velocity.y < 0)
+        {
+            if (m_isOnBlock)
+            {
+                m_anim.SetBool(ChacAnim.Jump.ToString(), false);
+                m_anim.SetBool(ChacAnim.Land.ToString(), true);
+            }
+            else
+            {
+                m_anim.SetBool(ChacAnim.Jump.ToString(), false);
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        IsOnBlock();
     }
 
     public bool IsComponentsNull()
     {
-        return m_rb == null || m_anim != null;
+        return m_rb == null || m_anim == null;
     }
     
-    private bool IsOnBlock()
+    private void IsOnBlock()
     {
         m_centerPos = new Vector3(transform.position.x,
-            transform.position.y - blockCheckingOffset,transform.position.z);
-
+                transform.position.y - blockCheckingOffset, transform.position.z
+                );
         Collider2D col = Physics2D.OverlapCircle(m_centerPos, blockCheckingRadius, blockLayer);
 
-        return col != null;
+        m_isOnBlock = col != null ? true : false;
+    }
+
+    public void Jump()
+    {
+        if (!GamepadController.Ins.CanJump || !m_isOnBlock || IsComponentsNull()) return;
+
+        GamepadController.Ins.CanJump = false;
+
+        m_rb.velocity = Vector2.up * jumpForce;
+
+        m_anim.SetBool(ChacAnim.Jump.ToString(), true);
+        m_anim.SetBool(ChacAnim.Land.ToString(), false);
+
+    }
+
+    public void BackToIdle()
+    {
+        m_anim.SetBool(ChacAnim.Land.ToString(), false);
+        m_anim.SetTrigger(ChacAnim.Idle.ToString());
     }
 
     private void OnDrawGizmos()
---
+++ Assets/Endless Game/Scripts/GamepadController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamepadController : MonoBehaviour
+{
+    public bool isOnMobile;
+    public static GamepadController Ins;
+
+    private bool m_canJump;
+
+    public bool CanJump { get => m_canJump; set => m_canJump = value; }
+
+    private void Awake()
+    {
+        Ins = this;
+    }
+
+    private void Update()
+    {
+        if (!isOnMobile)
+            m_canJump = Input.GetKeyDown(KeyCode.Space);
+    }
+}
